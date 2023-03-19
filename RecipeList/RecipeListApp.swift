76971ae387823d0a2daadb395fb0b58526994269
Nick Jüttner <nick@juni.io>
2023-03-19 14:58:44 +0100

    Initial Commit

---
+++ RecipeList/RecipeListApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeListApp.swift
+//  RecipeList
+//
+//  Created by Nick Jüttner on 19.03.23.
+//
+
+import SwiftUI
+
+@main
+struct RecipeListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
